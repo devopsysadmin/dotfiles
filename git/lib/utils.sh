7076dd19d33bf3d74c8d65fa4ec314138c659a36
David Pedersen <josedavid.pedersen.contractor@bbva.com>
2018-04-23 11:29:22 +0200

    Initial release. Git functions and os-based scripts

---
+++ git/lib/utils.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+git_current_branch(){
+    local ref=$(command git symbolic-ref --quiet HEAD 2> /dev/null)
+    local ret=$?
+    if [[ $ret != 0 ]]; then
+        [[ $ret == 128 ]] && return  # no git repo.
+        ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
+    fi
+    echo ${ref#refs/heads/}
+}
+
+git_commit_issue(){
+    local _branch="$1" ; shift
+    local _opts="$1" ; shift
+    local _message="$@"
+    local ISSUE=$(echo "$_branch" |\
+        sed -e 's/^feature\///g' -e 's/develop-//g' |\
+        sed -n 's/\(.*-[0-9]*\)-.*/\1/p')
+    if [[ $_opts =~ -.* ]]; then
+        echo -n ''
+    else
+        _message="$_opts"
+        _opts="-am"
+    fi
+    git commit $_opts "[$ISSUE] $_message"
+}
+
+git_pull_upstream(){
+    local _branch="$1"
+    git branch --set-upstream-to="origin/$_branch" "$_branch" ; git pull
+}
+
+BRANCH="$(git_current_branch)"
+case $(basename $0) in
+    gbc) echo "$BRANCH" ;;
+    gps) git push --set-upstream origin "$BRANCH" ;;
+    gls) git_pull_upstream "$BRANCH" ;;
+    git-clean-conflict) find . -name "*_BACKUP_*" -o -name "*_LOCAL_*" -o -name "*_BASE_*" -o -name "*_REMOTE_*" | xargs rm ;;
+    gci ) git_commit_issue "$BRANCH" "$@";;
+esac
