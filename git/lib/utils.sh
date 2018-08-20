8786113ef12f0fa686d670e343a1317e0f57a957
David Pedersen <josedavid.pedersen.contractor@bbva.com>
2018-08-20 14:43:42 +0200

    Git checkout/pull, git fetch and prune untracked, docker clean orphaned images/volumes

--- git/lib/utils.sh
+++ git/lib/utils.sh
@@ -35,12 +35,19 @@ git_branch_delete(){
     git branch -D "${_branch}" && git push origin :"${_branch}"
 }
 
+git_fetch_prune_local(){
+    git branch -vv | grep 'gone]' | awk '{print $1}' | xargs git branch -d
+}
+
 BRANCH="$(git_current_branch)"
 case $(basename $0) in
     gbc) echo "$BRANCH" ;;
     gbd) git_branch_delete "$1" ;;
     gps) git push --set-upstream origin "$BRANCH" ;;
+    gl) git pull $@ ;;
     gls) git_pull_upstream "$BRANCH" ;;
     git-clean-conflict) find . -name "*_BACKUP_*" -o -name "*_LOCAL_*" -o -name "*_BASE_*" -o -name "*_REMOTE_*" | xargs rm ;;
-    gci ) git_commit_issue "$BRANCH" "$@";;
+    gci) git_commit_issue "$BRANCH" "$@";;
+    gco) git checkout $@ ;;
+    gfp) git_fetch_prune_local ;;
 esac
