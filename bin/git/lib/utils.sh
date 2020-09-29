8b367a5494f37316f865224f58804a3e0296acfc
David Pedersen <josedavid.pedersen@bbva.com>
2020-09-29 13:25:12 +0200

    git-current-branch, gbd and gci as standalone utilities

--- bin/git/lib/utils.sh
+++ bin/git/lib/utils.sh
@@ -9,34 +9,9 @@ git_current_branch(){
     echo ${ref#refs/heads/}
 }
 
-git_commit_issue(){
-    local _branch="$1" ; shift
-    local _opts="$1" ; shift
-    local _message="$@"
-    local ISSUE=$(echo "$_branch" | sed -e 's/^feature\///g' -e 's/develop-//g')
-    if [[ $ISSUE =~ NO-ISSUE.* ]]; then
-    	ISSUE="NO-ISSUE"
-    else
-    	ISSUE=$(echo $ISSUE | sed -n 's/\(.*-[0-9]*\)-.*/\1/p')
-    fi
-    if [[ $_opts =~ ^-.* ]]; then
-        echo -n ''
-    else
-        _message="$_opts"
-        _opts="-am"
-    fi
-    git commit $_opts "[$ISSUE] $_message"
-}
-
 git_pull_upstream(){
     local _branch="$1"
     git branch --set-upstream-to="origin/$_branch" "$_branch" ; git pull
-}
-
-git_branch_delete(){
-    local _branch="$1"
-    git branch -D "${_branch}" || true
-    git push origin :"${_branch}"
 }
 
 git_fetch_prune_local(){ git branch -vv | grep 'gone]' | awk '{print $1}' | xargs git branch -d ;}
@@ -48,12 +23,10 @@ git_push_force(){ git push --force ;}
 BRANCH="$(git_current_branch)"
 case $(basename $0) in
     gbc) echo "$BRANCH" ;;
-    gbd) git_branch_delete "$1" ;;
     gps) git push --set-upstream origin "$BRANCH" ;;
     gl) git pull $@ ;;
     gls) git_pull_upstream "$BRANCH" ;;
     git-clean-conflict) find . -name "*_BACKUP_*" -o -name "*_LOCAL_*" -o -name "*_BASE_*" -o -name "*_REMOTE_*" | xargs rm ;;
-    gci) git_commit_issue "$BRANCH" "$@";;
     gco) git checkout $@ ;;
     gfp) git_fetch_prune_local ;;
 	gcd) git_commit_dirty ;;
